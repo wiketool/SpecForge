d61107ac153a35cc8e6b2f2b41888bb15fa0b96b
wiketool <64351271+wiketool@users.noreply.github.com>
2026-06-23 19:06:34 +0800

    update loss kernel block size limit

--- specforge/core/loss.py
+++ specforge/core/loss.py
@@ -25,11 +25,7 @@ def _calculate_settings(n):
     # reference: https://github.com/unslothai/unsloth/blob/fd753fed99ed5f10ef8a9b7139588d9de9ddecfb/unsloth/kernels/utils.py#L43
 
     MAX_FUSED_SIZE = 131072
-    BLOCK_SIZE = triton.next_power_of_2(n)
-    if BLOCK_SIZE > MAX_FUSED_SIZE:
-        raise RuntimeError(
-            f"Cannot launch Triton kernel since n = {n} exceeds the recommended Triton blocksize = {MAX_FUSED_SIZE}."
-        )
+    BLOCK_SIZE = min(triton.next_power_of_2(n), MAX_FUSED_SIZE)
 
     num_warps = 4
     if BLOCK_SIZE >= 32768:
